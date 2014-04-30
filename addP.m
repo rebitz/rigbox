24553e0b14947410b7df30d2a6ec0ff2c0b2619f
rebitz <rebitz@gmail.com>
2014-04-30 18:40:12 -0500

    removed pathdef

--- addP.m
+++
@@ -1,7 +0,0 @@
-% adds the first-order folders in its own directory to the path
-here = pwd;
-files = dir;
-directories = {files([files.isdir]).name};
-directories = directories(cellfun(@isempty,strfind(directories,'.')));
-tmp = strcat(here,'/',directories); % full directory name
-addpath(tmp{:},'-BEGIN') % add to beginning of path
