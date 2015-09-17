ca06fa6d314926f33c0a7998aa775e651718fccd
rebitz <rebitz@gmail.com>
2015-09-17 10:54:52 -0400

    second new rig

--- newComputers.m
+++ newComputers.m
@@ -1,6 +1,8 @@
-% [~,host] = system('hostname');
-% host = cellstr(host);
+[~,host] = system('hostname');
+host = cellstr(host);
 
 % oz is:
 % 'buschma-7f6pr52'
 
+% outer space is:
+% 'buschma-73gqr52'
